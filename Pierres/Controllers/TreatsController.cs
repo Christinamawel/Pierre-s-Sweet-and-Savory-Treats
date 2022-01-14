7234bcabdb4b4addd0c53f85da2337173aea7b7b
Christinamawel <christinamawel@gmail.com>
2022-01-14 10:02:31 -0800

    Add Details View to Treats

--- Pierres/Controllers/TreatsController.cs
+++ Pierres/Controllers/TreatsController.cs
@@ -54,11 +54,17 @@ namespace Pierres.Controllers
     }
 
     [HttpPost]
-    public ActionResult Edit(Treat Treat, int CategoryId)
+    public ActionResult Edit(Treat Treat)
     {
       _db.Entry(Treat).State = EntityState.Modified;
       _db.SaveChanges();
       return RedirectToAction("Index");
+    }
+
+    public ActionResult Details (int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(Treat => Treat.TreatId == id);
+      return View(thisTreat);
     }
   }
 }
