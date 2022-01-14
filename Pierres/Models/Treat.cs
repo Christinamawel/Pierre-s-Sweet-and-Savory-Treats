811b2de521c3297c79c9a6e39e1dd9810381af18
Christinamawel <christinamawel@gmail.com>
2022-01-14 08:49:06 -0800

    add Index view for Home

---
+++ Pierres/Models/Treat.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace Pierres.Models
+{
+  public class Treat
+  {
+    public Treat()
+    {
+      
+    }
+
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+    public int Rating { get; set; }
+
+  }
+}
