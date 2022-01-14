ade2bf86a300cd0b70c90d7259de489d5a315cf5
Christinamawel <christinamawel@gmail.com>
2022-01-14 10:05:52 -0800

    add Delete view to Treats

--- Pierres/Controllers/TreatsController.cs
+++ Pierres/Controllers/TreatsController.cs
@@ -66,5 +66,20 @@ namespace Pierres.Controllers
       var thisTreat = _db.Treats.FirstOrDefault(Treat => Treat.TreatId == id);
       return View(thisTreat);
     }
+
+    public ActionResult Delete (int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(Treats => Treats.TreatId == id);
+      return View(thisTreat);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(Treats => Treats.TreatId == id);
+      _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
