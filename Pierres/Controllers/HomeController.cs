a9ade71f3bd45b58bb5dcacba37500950c9ed2fc
Christinamawel <christinamawel@gmail.com>
2022-01-17 12:44:19 -0800

    update home index to show all shared recipes

--- Pierres/Controllers/HomeController.cs
+++ Pierres/Controllers/HomeController.cs
@@ -1,13 +1,33 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Pierres.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Pierres.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierresContext _db;
+
+    public HomeController(PierresContext db)
+    {
+      _db = db;
+    }
+
+
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      List<Treat> model = _db.Treats.Where(entry => entry.Shared == true).OrderByDescending(x => x.Rating).ToList();
+      return View(model);
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(Treat => Treat.TreatId == id);
+      return View(thisTreat);
     }
   }
 }
